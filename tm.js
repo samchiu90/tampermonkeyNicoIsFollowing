4c824db17678e1d4b16b55c28eb8c6eadfe80787
samchiu90 <51096971+samchiu90@users.noreply.github.com>
2022-03-14 16:37:05 +0800

    Update tm.js

--- tm.js
+++ tm.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.nicovideo.jp/
+// @match        *://www.nicovideo.jp/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nicovideo.jp
 // @grant        none
 // ==/UserScript==
